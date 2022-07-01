74eeacb05abbbcf21446715d3e88415e0ece0125
eralpcalhan <eralpcalhan@gmail.com>
2022-07-01 16:56:56 +0100

    01/07 UPDATES

---
+++ Interns_PCBs/pendulum_daughterboard/LED_Test_Firmware/LED_Test_Firmware.ino
@@ -0,0 +1,110 @@
+int STU_LED_1 = 6;
+int STU_LED_2 = 7;
+int STU_LED_3 = 8;
+int SUP_LED_1 = 10;
+int SUP_LED_2 = 2;
+int SUP_LED_3 = 9;
+int STU_ENABLE = 5;
+int DRIVE_IN = 12;
+int LOAD_IN = 4;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode (STU_LED_1, INPUT);
+  pinMode (STU_LED_2, INPUT);
+  pinMode (STU_LED_3, INPUT);
+  pinMode (SUP_LED_1, INPUT);
+  pinMode (SUP_LED_2, INPUT);
+  pinMode (SUP_LED_3, INPUT);
+  pinMode (STU_ENABLE, INPUT);
+  pinMode (DRIVE_IN, INPUT);
+  pinMode (LOAD_IN, INPUT);
+
+
+  digitalWrite (STU_LED_1, LOW);
+  digitalWrite (STU_LED_2, LOW);
+  digitalWrite (STU_LED_3, LOW);
+  digitalWrite (SUP_LED_1, LOW);
+  digitalWrite (SUP_LED_2, LOW);
+  digitalWrite (SUP_LED_3, LOW);
+  digitalWrite (STU_ENABLE, LOW);
+  digitalWrite (DRIVE_IN, LOW);
+  digitalWrite (LOAD_IN, LOW);
+
+flashLED();
+}
+
+
+
+void loop() {
+  // Initially setting all the pins low
+  digitalWrite (STU_LED_1, LOW);
+  digitalWrite (STU_LED_2, LOW);
+  digitalWrite (STU_LED_3, LOW);
+  digitalWrite (SUP_LED_1, LOW);
+  digitalWrite (SUP_LED_2, LOW);
+  digitalWrite (SUP_LED_3, LOW);
+  digitalWrite (STU_ENABLE, LOW);
+  digitalWrite (DRIVE_IN, LOW);
+  digitalWrite (LOAD_IN, LOW);
+
+  // Trying to turn the pins on without enable
+
+  delay(3000);
+  digitalWrite (STU_LED_1, HIGH);
+  delay(1000);
+  digitalWrite (STU_LED_1,  LOW);
+  digitalWrite (STU_LED_2, HIGH);
+  delay(1000);
+  digitalWrite (STU_LED_2, LOW);
+  digitalWrite (STU_LED_3, HIGH);
+  delay(1000);
+  digitalWrite (STU_LED_3, LOW);
+  digitalWrite (SUP_LED_1, HIGH);
+  delay(1000);
+  digitalWrite (SUP_LED_1, LOW);
+  digitalWrite (SUP_LED_2, HIGH);
+  delay(1000);
+  digitalWrite (SUP_LED_2, LOW);
+  digitalWrite (SUP_LED_3, HIGH);
+
+  // Trying to turn the pins on with enable
+  delay(1000);
+  digitalWrite (STU_ENABLE, HIGH);
+  delay(1000);
+  digitalWrite (SUP_LED_3, LOW);
+  digitalWrite (STU_LED_1, HIGH);
+  delay(1000);
+  digitalWrite (STU_LED_1,  LOW);
+  digitalWrite (STU_LED_2, HIGH);
+  delay(1000);
+  digitalWrite (STU_LED_2, LOW);
+  digitalWrite (STU_LED_3, HIGH);
+  delay(1000);
+  digitalWrite (STU_LED_3, LOW);
+  digitalWrite (SUP_LED_1, HIGH);
+  delay(1000);
+  digitalWrite (SUP_LED_1, LOW);
+  digitalWrite (SUP_LED_2, HIGH);
+  delay(1000);
+  digitalWrite (SUP_LED_2, LOW);
+  digitalWrite (SUP_LED_3, HIGH);
+
+
+
+}
+
+
+void flashLED() {
+  int count = 6;
+  while (count) {
+    digitalWrite (SUP_LED_1, HIGH);
+    delay(500);
+    digitalWrite (SUP_LED_1,  LOW);
+    delay(200);
+    count--;
+  }
+}
