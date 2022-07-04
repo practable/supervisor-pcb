4c81e67fe2ff974e2a58c6ba19a769f8e20fac9c
eralpcalhan <eralpcalhan@gmail.com>
2022-07-04 11:26:01 +0100

    Changed Logic

--- Interns_PCBs/pendulum_daughterboard/LED_Test_Firmware/LED_Test_Firmware.ino
+++ Interns_PCBs/pendulum_daughterboard/LED_Test_Firmware/LED_Test_Firmware.ino
@@ -13,85 +13,75 @@ int LOAD_IN = 4;
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode (STU_LED_1, INPUT);
-  pinMode (STU_LED_2, INPUT);
-  pinMode (STU_LED_3, INPUT);
-  pinMode (SUP_LED_1, INPUT);
-  pinMode (SUP_LED_2, INPUT);
-  pinMode (SUP_LED_3, INPUT);
-  pinMode (STU_ENABLE, INPUT);
-  pinMode (DRIVE_IN, INPUT);
-  pinMode (LOAD_IN, INPUT);
+  pinMode (STU_LED_1, OUTPUT);
+  pinMode (STU_LED_2, OUTPUT);
+  pinMode (STU_LED_3, OUTPUT);
+  pinMode (SUP_LED_1, OUTPUT);
+  pinMode (SUP_LED_2, OUTPUT);
+  pinMode (SUP_LED_3, OUTPUT);
+  pinMode (STU_ENABLE, OUTPUT);
+  pinMode (DRIVE_IN, OUTPUT);
+  pinMode (LOAD_IN, OUTPUT);
 
 
-  digitalWrite (STU_LED_1, LOW);
-  digitalWrite (STU_LED_2, LOW);
-  digitalWrite (STU_LED_3, LOW);
-  digitalWrite (SUP_LED_1, LOW);
-  digitalWrite (SUP_LED_2, LOW);
-  digitalWrite (SUP_LED_3, LOW);
-  digitalWrite (STU_ENABLE, LOW);
-  digitalWrite (DRIVE_IN, LOW);
-  digitalWrite (LOAD_IN, LOW);
-
-flashLED();
+//flashLED();
 }
 
 
 
 void loop() {
-  // Initially setting all the pins low
-  digitalWrite (STU_LED_1, LOW);
-  digitalWrite (STU_LED_2, LOW);
-  digitalWrite (STU_LED_3, LOW);
-  digitalWrite (SUP_LED_1, LOW);
-  digitalWrite (SUP_LED_2, LOW);
-  digitalWrite (SUP_LED_3, LOW);
-  digitalWrite (STU_ENABLE, LOW);
-  digitalWrite (DRIVE_IN, LOW);
-  digitalWrite (LOAD_IN, LOW);
+//Initially setting all the pins low
+ digitalWrite (STU_LED_1, LOW);
+ digitalWrite (STU_LED_2, LOW);
+ digitalWrite (STU_LED_3, LOW);
+ digitalWrite (SUP_LED_1, LOW);
+ digitalWrite (SUP_LED_2, LOW);
+ digitalWrite (SUP_LED_3, HIGH);
+ digitalWrite (STU_ENABLE, LOW);
+ digitalWrite (DRIVE_IN, LOW);
+ digitalWrite (LOAD_IN, LOW);
 
   // Trying to turn the pins on without enable
 
-  delay(3000);
-  digitalWrite (STU_LED_1, HIGH);
-  delay(1000);
-  digitalWrite (STU_LED_1,  LOW);
-  digitalWrite (STU_LED_2, HIGH);
-  delay(1000);
-  digitalWrite (STU_LED_2, LOW);
-  digitalWrite (STU_LED_3, HIGH);
-  delay(1000);
-  digitalWrite (STU_LED_3, LOW);
-  digitalWrite (SUP_LED_1, HIGH);
-  delay(1000);
-  digitalWrite (SUP_LED_1, LOW);
-  digitalWrite (SUP_LED_2, HIGH);
-  delay(1000);
-  digitalWrite (SUP_LED_2, LOW);
-  digitalWrite (SUP_LED_3, HIGH);
+  //delay(3000);
+//  digitalWrite (STU_LED_1, HIGH);
+ // delay(1000);
+  //digitalWrite (STU_LED_1,  LOW);
+  //digitalWrite (STU_LED_2, HIGH);
+ // delay(1000);
+  //digitalWrite (STU_LED_2, LOW);
+  //digitalWrite (STU_LED_3, HIGH);
+  //delay(1000);
+  //digitalWrite (STU_LED_3, LOW);
+  //digitalWrite (SUP_LED_1, HIGH);
+  //delay(1000);
+  //digitalWrite (SUP_LED_1, LOW);
+  //digitalWrite (SUP_LED_2, HIGH);
+ // delay(1000);
+  //digitalWrite (SUP_LED_2, LOW);
+ // digitalWrite (SUP_LED_3, HIGH);
 
   // Trying to turn the pins on with enable
-  delay(1000);
-  digitalWrite (STU_ENABLE, HIGH);
-  delay(1000);
-  digitalWrite (SUP_LED_3, LOW);
-  digitalWrite (STU_LED_1, HIGH);
-  delay(1000);
-  digitalWrite (STU_LED_1,  LOW);
-  digitalWrite (STU_LED_2, HIGH);
-  delay(1000);
-  digitalWrite (STU_LED_2, LOW);
-  digitalWrite (STU_LED_3, HIGH);
-  delay(1000);
-  digitalWrite (STU_LED_3, LOW);
-  digitalWrite (SUP_LED_1, HIGH);
-  delay(1000);
-  digitalWrite (SUP_LED_1, LOW);
-  digitalWrite (SUP_LED_2, HIGH);
-  delay(1000);
-  digitalWrite (SUP_LED_2, LOW);
-  digitalWrite (SUP_LED_3, HIGH);
+ // delay(1000);
+ // digitalWrite (STU_ENABLE, HIGH);
+ // delay(1000);
+ // digitalWrite (SUP_LED_3, LOW);
+//  digitalWrite (STU_LED_1, HIGH);
+ // delay(1000);
+//  digitalWrite (STU_LED_1,  LOW);
+//  digitalWrite (STU_LED_2, HIGH);
+//  delay(1000);
+ // digitalWrite (STU_LED_2, LOW);
+ // digitalWrite (STU_LED_3, HIGH);
+ // delay(1000);
+ // digitalWrite (STU_LED_3, LOW);
+ // digitalWrite (SUP_LED_1, HIGH);
+ // delay(1000);
+ // digitalWrite (SUP_LED_1, LOW);
+ // digitalWrite (SUP_LED_2, HIGH);
+ // delay(1000);
+ // digitalWrite (SUP_LED_2, LOW);
+  //digitalWrite (SUP_LED_3, HIGH);
 
 
 
